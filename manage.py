8f9cb95c20ae25e61896b7279eccb7cbcd448294
Jerzy Pawlikowski <jurekpawlikowski@gmail.com>
2017-03-18 22:27:39 +0100

    add proper commands

--- manage.py
+++ manage.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from flask_script import Manager
+
 from app.factory import create_app
 
 app = create_app()
 
-if __name__ == "main":
-    app.run()
+
+def main():
+    manager = Manager(app=app)
+    manager.run()
+
+if __name__ == "__main__":
+    main()
