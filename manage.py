cb69c45d69c70832811702734156cb63033937ca
Jerzy Pawlikowski <jurekpawlikowski@gmail.com>
2017-03-19 10:16:47 +0100

    command to build frontend

--- manage.py
+++ manage.py
@@ -9,6 +9,18 @@ app = create_app()
 
 def main():
     manager = Manager(app=app)
+
+    @manager.command
+    def build_frontend():
+        import os
+        from subprocess import Popen
+        node_directory = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)),
+            "app/static/js"
+        )
+        p = Popen("npm run build", cwd=node_directory, shell=True)
+        p.wait()
+
     manager.run()
 
 if __name__ == "__main__":
